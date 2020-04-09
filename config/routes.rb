df37ca26ecfb6e062ba8eef79542bae699d570b3
tatsuya <ta2831jo07@gmail.com>
2020-04-09 09:54:10 +0000

    rails02 完

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  
   root 'pages#index'
   get 'pages/help'
+  
+  resources :users
 
 end
