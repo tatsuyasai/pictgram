8a88e3bcaac2a75c8d67145d2a8226c34d2567f8
tatsuya <ta2831jo07@gmail.com>
2020-04-15 04:53:46 +0000

    rails04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name , presence: true
-  validates :email , presence: true
+  validates :name , presence: true , length: {maximum: 15}
+  validates :email , presence: true , format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   
   has_secure_password
+  
+  validates :password , length: {minimum: 8 , maximum: 32} , format: {with: /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/}
+  validates :password_confirmation , length: {minimum: 8 , maximum: 32} 
 end
