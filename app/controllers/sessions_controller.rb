b4a3ba3db6bab09b6d7c2b664e37cfca2036cc46
tatsuya <ta2831jo07@gmail.com>
2020-04-17 04:06:41 +0000

    課題1まで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
@@ -13,8 +13,23 @@ class SessionsController < ApplicationController
     end
   end
   
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end 
+  
   private
   def log_in(user)
     session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end 
+  
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
