9c8eec1828aec7fe048d56f65177d292dca3bc73
tatsuya <ta2831jo07@gmail.com>
2020-04-28 10:43:44 +0000

    テスト完

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,21 @@
 class CommentsController < ApplicationController
   def new
     @topic = Topic.find(params[:topic_id])
-    @comment = Comment.new
+    @comment = @topic.comments.new
   end
   
   def create
-    @comment = current_user.comments.new(comment_params)
-   
-    
-    if @comment.save
-      redirect_to topics_path, success: 'コメントを投稿しました'
+    comment = Comment.create(create_params)
+
+    if comment.persisted?
+      redirect_to topics_path, success: 'コメントの投稿に成功しました'
     else
-      flash.now[:danger] = 'コメントの投稿に失敗しました'
-      render :new
+      redirect_to new_topic_comment_path, danger: 'コメントの投稿に失敗しました'
     end
   end
-    
-    private
-    def comment_params
-      params.require(:comment).permit(:content, :topic_id, :user_id)
-    end
+
+  private
+  def create_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, topic_id: params[:topic_id])
+  end
 end
